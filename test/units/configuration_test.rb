6d3782c0413ac9a0926fdf38251225875eb37d24
TJ Taylor <Thomas_Taylor2@cable.comcast.com>
2013-04-16 11:46:14 -0600

    Enrar.gem_root is constant

--- test/units/configuration_test.rb
+++ test/units/configuration_test.rb
@@ -1,6 +1,26 @@
 require 'test_helper'
 
 describe 'configurations' do
+  describe Enrar, '.root' do
+    it "points to the user's project's root" do
+      Enrar.root.to_s.must_equal File.expand_path('../../../', __FILE__)
+    end
+  end
+
+  describe Enrar, '.gem_root' do
+    it "points to Enrar's root" do
+      Enrar.root.to_s.must_equal File.expand_path('../../../', __FILE__)
+    end
+
+    it "stays the same even if I change directory" do
+      old_root = Enrar.root
+      Dir.chdir('/') do
+        Enrar.root.must_equal old_root
+      end
+    end
+  end
+
+
   describe Enrar, '.root=' do
     def new_root 
       File.expand_path('../../fixtures/test_project', __FILE__)
@@ -14,12 +34,6 @@ describe 'configurations' do
     it "creates it as a Pathname" do
       Enrar.root = new_root
       Enrar.root.must_be_kind_of Pathname
-    end
-  end
-
-  describe Enrar, '.root' do
-    it "points to the user's project's root" do
-      Enrar.root.to_s.must_equal File.expand_path('../../../', __FILE__)
     end
   end
 
