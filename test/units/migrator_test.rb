c6e4cea55c9b2332a4a27547acf41f779a22fa34
TJ Taylor <Thomas_Taylor2@cable.comcast.com>
2013-04-17 21:24:51 -0600

    Properly named migration files for migrator_test.

--- test/units/migrator_test.rb
+++ test/units/migrator_test.rb
@@ -12,9 +12,13 @@ describe Enrar::Migrator do
       Dir.chdir @test_directory
       Enrar.initialize!
 
-      sample_migration = File.read(File.expand_path('../../fixtures/migration.sample.rb', __FILE__))
-      migration = Enrar::Migration.new('create_tests').generate!
-      File.write(migration.path, sample_migration)
+      @migration_versions = []
+      %w(create_tests create_posts create_users).each do |migration_name|
+        sample = File.read(File.expand_path("../../fixtures/#{migration_name}.rb", __FILE__))
+        migration = Enrar::Migration.new(migration_name).generate!
+        File.write(migration.path, sample)
+        @migration_versions << migration.version
+      end
     end
 
     after(:each) do
@@ -22,9 +26,16 @@ describe Enrar::Migrator do
       Dir.chdir @old_directory
     end
 
-    it 'runs the migrations in the migration_dir' do
+    it 'runs all migrations in the migration_dir' do
       Enrar::Migrator.new(nil, verbose: false).migrate
       ActiveRecord::Base.connection.table_exists?('tests').must_equal true
+    end
+
+    it 'accepts a migration version' do
+      Enrar::Migrator.new(@migration_versions.first.to_i, verbose: false).migrate
+      ActiveRecord::Base.connection.table_exists?('tests').must_equal true
+      ActiveRecord::Base.connection.table_exists?('posts').must_equal false, 'Posts WAS created'
+      ActiveRecord::Base.connection.table_exists?('users').must_equal false, 'Users WAS created'
     end
   end
 end
